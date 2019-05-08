201175828ac3a059c49d04dcfafb79da0b0a3e22
Michael Frenkel <michael.frenkel@outlook.com>
2019-05-08 14:49:26 -0700

    setup file strucuture, init package.json, install project dependencies

---
+++ challenge_1/client/components/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import Search from './Search.jsx';
+import EventList from './EventList.jsx';
+import ReactPaginate from 'react-paginate';
+import axios from 'axios';
+
+class App extends Component {
+  
+}
