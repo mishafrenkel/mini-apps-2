8db6b757cc21f097d68fef62abc4f280d963e561
Michael Frenkel <michael.frenkel@outlook.com>
2019-05-09 16:12:28 -0700

    Init

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 5000;
 
+const coindesk = require('./data/coindesk.js');
 
+app.use(express.static(__dirname + "./public"));
+
+app.get('/prices', (req, res) => {
+  coindesk.getPrice((err, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Succesful API call to grab prices! ----> BY PRICE');
+      res.send(result);
+    }
+  });
+});
+
+app.get('/dates', (req, res) => {
+  coindesk.getPriceByDates((err, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(result);
+      console.log('Successful API call ---> BY DATE')
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Express server is running at port ${port}`)
+});
