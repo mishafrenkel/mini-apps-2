e9019f11b6a76259fa087621eef63cd211e9c370
Michael Frenkel <michael.frenkel@outlook.com>
2019-05-10 11:29:41 -0700

    update server.js with  cors()

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,24 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+})
 
