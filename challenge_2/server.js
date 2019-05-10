9ce3cdb7969d544cf1b67c0a9f3b799af3226fbf
Michael Frenkel <michael.frenkel@outlook.com>
2019-05-09 19:21:11 -0700

    implement chart, mount in index and style with flex display

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,36 +1,11 @@
-
+const path = require('path');
 const express = require('express');
+
+const PORT = 5000;
 const app = express();
-const port = 5000;
 
-const coindesk = require('./data/coindesk.js');
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + "./public"));
-
-app.get('/prices', (req, res) => {
-  coindesk.getPrice((err, result) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Succesful API call to grab prices! ----> BY PRICE');
-      res.send(result);
-    }
-  });
-});
-
-app.get('/dates', (req, res) => {
-  coindesk.getPriceByDates((err, result) => {
-    if (error) {
-      console.log(error);
-    } else {
-      res.send(result);
-      console.log('Successful API call ---> BY DATE')
-    }
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Express server is running at port ${port}`)
-});
-
-
+app.listen(PORT, () => { console.log(`Server listening in Port ${PORT}`); });
--- challenge_2/webpack.config.js
+++ challenge_2/webpack.config.js
@@ -1,22 +1,37 @@
 const path = require('path');
-const SRC_DIR = path.join(__dirname, "/client");
-const DIST_DIR = path.join(__dirname, "/public");
+const webpack = require('webpack');
+const SRC_DIR = path.join(__dirname, '/client');
+const DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
-  entry: `${SRC_DIR}/app.jsx`,
+  entry: `${SRC_DIR}/index.jsx`,
+  devtool: 'source-map',
+  mode: 'development',
+  cache: true,
   output: {
-    filename: "bundle.js",
+    filename: 'bundle.js',
     path: DIST_DIR
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
   module: {
     rules: [
       {
+        test: /\.(js|jsx)?/,
         include: SRC_DIR,
-        loader: "babel-loader",
-        query: {
-          presets: ["env", "react"]
+        use: {
+          loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        include: SRC_DIR,
+        use: [ { loader: 'style-loader' }, { loader: 'css-loader' }]
       }
-    ],
-  }
+    ]
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+  ],
 };
