e07c5fc76844123fbb38b4fec45ebdc5fecd6200
Michael Frenkel <michael.frenkel@outlook.com>
2019-05-11 17:37:31 -0700

    implement root App component - functional stateless

--- challenge_3/client/components/App.jsx
+++ challenge_3/client/components/App.jsx
@@ -1,2 +1,14 @@
 import React from "react";
 import axios from "axios";
+import ScoreCard from "./components/Scorecard"
+
+const App = () => {
+  return (
+    <div>
+      Bowling Score Card Tracker
+      <Scorecard />
+    </div>
+  );
+};
+
+export default App;
