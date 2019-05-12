784de57a429fdaa5e56237f5311156d6276cd584
Michael Frenkel <michael.frenkel@outlook.com>
2019-05-11 17:26:39 -0700

    spin up express server on port 8000

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const bluebird = require('bluebird');
 
+const app = express();
+bluebird.promisifyAll(app);
+
+const PORT = 8000;
+
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, () => {
+  console.log(`Connected to server and listening on port: ${PORT}`);
+});
 
