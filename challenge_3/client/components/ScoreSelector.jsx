45d1498691adf0ce4b1674064d23e232794af210
Michael Frenkel <michael.frenkel@outlook.com>
2019-05-11 18:20:47 -0700

    implement and nest Button component for keypad values into container component

---
+++ challenge_3/client/components/ScoreSelector.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ScoreButtonRow from "./KeyPadRow";
+
+const ScoreSelector = function({ onKeyPadPress }) {
+  const keyPadValues = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ["Spare", 0, "Strike"]];
+
+  return (
+    <div id="keypad">
+      <table border="1">
+        <tbody>
+          {keyPadValues.map((row, index) => (
+            <ScoreButtonRow
+              row={row}
+              key={`row${index}`}
+              onKeyPadPress={onKeyPadPress}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ScoreSelector;
